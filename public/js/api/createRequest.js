e7e6b6836d57c70a671af19184d179f1dea22155
Valentin Korneev <v.korneef@gmail.com>
2022-01-13 22:57:25 +0500

    debugging and settings

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -2,6 +2,32 @@
  * Основная функция для совершения запросов
  * на сервер.
  * */
+
+//  createRequest({
+//     url: 'https://example.com', // адрес
+//     data: { // произвольные данные, могут отсутствовать
+//       email: 'ivan@poselok.ru',
+//       password: 'odinodin'
+//     },
+//     method: 'GET', // метод запроса
+//     /*
+//       Функция, которая сработает после запроса.
+//       Если в процессе запроса произойдёт ошибка, её объект
+//       должен быть в параметре err.
+//       Если в запросе есть данные, они должны быть переданы в response.
+//     */
+//     callback: (err, response) => {
+//       console.log( 'Ошибка, если есть', err );
+//       console.log( 'Данные, если нет ошибки', response );
+//     }
+//   });
+
 const createRequest = (options = {}) => {
+    const xhr = new XMLHttpRequest();
+    xhr.responseType = 'json'
+    xhr.open(options.method, options.url);
+
 
 };
+
+asdf
